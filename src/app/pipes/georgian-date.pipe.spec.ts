ae209b1d353fd8dff49201eb79b68f44216a57dd
mmode5 <maiko.mode05@gmail.com>
2025-03-21 01:52:00 -0400

    Auto-generated commit

--- src/app/pipes/georgian-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GeorgianDatePipe } from './georgian-date.pipe';
-
-describe('GeorgianDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GeorgianDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/store/filter.action.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const setFilters = createAction(
-  '[Filter] Set Filters',
-  props<{ filters: { [key: string]: any[] } }>()
-);
-
-export const clearFilters = createAction('[Filter] Clear Filters');
